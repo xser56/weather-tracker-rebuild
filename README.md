af0cac0bcbaa42334a51ffe18c7d4cd57f54c3ea
xser56 <gettosheep56@gmail.com>
2025-03-31 05:25:58 -0700

    Update REadMe

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Name: Andrew Sayasing
 Date: 3/31/25
-Project Name: 
+Project Name: Weather App Rebuild
 Description: Built my original weather app with React, Next.Js, Typescript and Tailwind. Extremely late turn in and no peer review
 Peer Review: None
--- src/app/Service/API.ts
+++ src/app/Service/API.ts
@@ -1,6 +1,5 @@
 import { CurrentDate } from "../Interfaces/currentTime";
-
-const myKey = "912423fbae3a70a3af91a65f583a662f";
+import { myKey } from "./key";
 
 export const grabWeatherAPI = async (city: string) => {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${myKey}`);
